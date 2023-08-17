1f034bb0be76723b8e8401db0aa5abcd347764ca
Energetik04 <137441615+Energetik04@users.noreply.github.com>
2023-08-17 18:41:52 +0300

    изм1

--- src/edu/itacademy/test1/Task8.java
+++ src/edu/itacademy/test1/Task8.java
@@ -6,16 +6,18 @@ public class Task8 {
 		double x;
 		double result;
 		
-		x=0.1;
-		result=0;
-		while(x<=10){
-			result=Math.pow(x, 2)-Math.pow(Math.E, 2*x)+4;
-			if (result==0){
+		x = 0.1;
+		result = 1;
+		while (x <= 10) {
+			result = Math.pow(x, 2) - Math.pow(Math.E, 2 * x) + 4;
+			if (result == 0) {
 				break;
 			}
-			x=x+0.1;
-			}
-		System.out.println(result);
+			x = x + 0.1;
 		}
-
+		if (result != 0) {
+			System.out.print("Нуль фунции не найден");
+		}
 	}
+
+}
